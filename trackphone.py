d79fd4a5a138e5ecc26cb023409c785197a4cd94
Mehmet Can Özcan <106785715+mcozcan@users.noreply.github.com>
2023-03-22 16:26:10 +0300

    Create trackphone.py

---
+++ trackphone.py
@@ -0,0 +1,10 @@
+# you must install
+# pip install geocoder
+# pip install phonenumbers      
+import phonenumbers
+from phonenumbers import geocoder
+#forexample +90598569845
+phone_number1 = phonenumbers.parse("+phonenumber") 
+
+
+print(geocoder.description_for_number(phone_number1,"tr"))
